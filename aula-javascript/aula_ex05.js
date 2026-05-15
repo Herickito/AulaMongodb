2f4dfaf23e81e1d5425b9689c449cabae1805ea6
Herick Henrique Gomes Silva <166627507+Herickito@users.noreply.github.com>
2026-05-15 23:21:40 +0000

    aaaaamongo

---
+++ aula-javascript/aula_ex05.js
@@ -0,0 +1,53 @@
+/*const numeros = [1,2,3,4,5];
+
+numeros.forEach((num,index) => {
+    console.log(`Numero: ${num} endereco: ${index}`);
+    
+});
+
+
+const numeros2 = [10,20,30,40,50];
+const maiorQue15 = numeros2.filter(num => num >15);
+
+console.log(numeros2);
+console.log(maiorQue15);
+
+
+
+const numeros2 = [10,20,30,40,50];
+const maiorQue15 = numeros2.filter(num => num >15);
+
+const dobrados = numeros2.map(num => num*2);
+
+console.log(numeros2);
+console.log(dobrados);
+
+const numeros3 = [5,10,15,20];
+
+const maiorQue10 = numeros3.find(num => num > 10);
+console.log(maiorQue10);
+
+
+const numeros4 = [10,20,30,40];
+
+const todosMaioresQue8 = numeros4.every(num => num >8);
+console.log(todosMaioresQue8);
+
+const numeros5 = [5,8,9,10,12];
+
+const todosMaiorQue10 = numeros5.some(num => num >10);
+console.log(todosMaiorQue10);
+
+
+
+const numeros5 = [5,8,9,10,12];
+
+const todosMaiorQue10 = numeros5.some(num => num >10);
+console.log(todosMaiorQue10);
+*/
+
+const numeros6 = [1,2,3,4,5];
+
+const soma = numeros6.reduce((acumulador, atual) => acumulador + atual, 0);
+
+console.log(soma);
